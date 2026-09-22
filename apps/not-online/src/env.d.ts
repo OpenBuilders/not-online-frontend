8d8f312a43ea029fd062d06371b37d48f0130b94
mkovza <michaelkovzanovich@gmail.com>
2026-09-22 10:41:30 +0800

    fix: embed public plausible configuration

--- apps/not-online/src/env.d.ts
+++ apps/not-online/src/env.d.ts
@@ -2,8 +2,6 @@
 
 interface ImportMetaEnv {
   readonly LINK_PAGES_API_URL?: string;
-  readonly PUBLIC_PLAUSIBLE_HOST?: string;
-  readonly PUBLIC_PLAUSIBLE_DOMAIN?: string;
 }
 
 interface ImportMeta {
